b88f594c2039a5fd941b967c7afbc889212a6320
pavanmandapaka <pavansaimandapaka.com>
2025-08-23 14:35:50 +0530

    requirements added

---
+++ MedicalChatbot/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MedicalChatbot",
+    version="0.1.0",
+    author="Pavan Mandapaka",
+    author_email="pavansaimandapaka@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
